939d8f9c9add718e41ecec3d2b18a1fcfe5d350a
mgdev15 <miguelpilonetto8@gmail.com>
2025-07-30 23:15:00 -0300

    Lancamento Vertical

--- lancVtcCalc/lancamentoVtc.js
+++ lancVtcCalc/lancamentoVtc.js
@@ -103,14 +103,20 @@ const proxBtn = document.getElementById("proxBtn");
 proxBtn.addEventListener("click", () => {
 
     perguntaIdx++;
+    window.scrollTo(0, 245);
 
+    pgElement.classList.remove('displaying');
+    pgElement.offsetHeight;
+    
     if(perguntasLancamentoVtc.length > perguntaIdx){
         pgElement.src = perguntasLancamentoVtc[perguntaIdx].source;
+        pgElement.classList.add('displaying');
         proxBtn.innerHTML = `<i class="fa-solid fa-atom"></i> Próxima questão <i class="fa-solid fa-atom"></i>` ;
     }
     else{
         perguntaIdx = 0;
         pgElement.src = perguntasLancamentoVtc[perguntaIdx].source;
+        pgElement.classList.add('displaying');
     }
 
 });
